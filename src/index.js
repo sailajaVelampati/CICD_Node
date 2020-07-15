580ec464faeab6d72510ff64dd6f58a385efe237
sailaja velmapati <sailajasmart@gmail.com>
2020-07-15 13:22:58 +0530

    View Content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 var express = require('Express');
 const bodyParser = require('body-parser');
+const path = require('path');
 var app = express();
 let port = process.env.PORT || 3000;
 var products = require('./routes');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Account Summary' });
+});
 
 app.use('/productservice', products);
 
